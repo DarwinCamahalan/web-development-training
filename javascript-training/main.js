351cbdeae603a4b5ebd2476a4986ac7f92d0853d
DarwinCamahalan <camahalandarwin@gmail.com>
2023-09-25 10:41:47 +0800

    Start of Javascript learning

--- javascript-training/main.js
+++ javascript-training/main.js
@@ -1,3 +1,116 @@
+// DOM ONLY
+
+/*
+// single element
+const form = document.getElementById('my-form')
+console.log(form)
+console.log(document.querySelector('h1'))
+
+// multiple elements
+console.log(document.querySelectorAll('.item'))
+console.log(document.getElementsByClassName('.item'))
+console.log(document.getElementsByTagName('li'))
+
+
+const items = document.querySelectorAll('.item')
+
+items.forEach((item) => console.log(item))
+*/
+
+const ul = document.querySelector('.items')
+
+ul.firstElementChild.textContent = 'Hello'
+ul.children[1].innerText = 'Brad'
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONSOLE ONLY
+
 /* 
 
 // Data Types
@@ -73,5 +186,130 @@ const todos = [
     },
 ]
 
+// const todoJSON = JSON.stringify(todos)
+// console.log(todoJSON)
 
-console.log(todos[1].text)
+/*
+// For Loop
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+// While Loop
+let i = 0
+
+while(i < 10){
+    console.log(i)
+    i++
+}
+
+*/
+
+/*
+// forEach, map, filter
+const todoCompleted = todos.filter(function(todo){
+    return todo.isCompleted === true
+}).map(function(todo){
+    return todo.text
+})
+
+console.log(todoCompleted)
+*/
+
+
+/*
+const x = 10
+
+// non strict in type
+if (x == 10) {
+    console.log('x is 10')
+}
+
+// strict in type int to int
+if (x === 10) {
+    console.log('x is 10')
+}
+*/
+
+
+/*
+// ternary
+const y = 11
+
+const color = y > 10 ? 'red' : 'blue'
+
+switch (color) {
+    case 'red':
+        console.log('color is red')
+        break;
+    case 'blue':
+        console.log('color is blue')
+    default:
+        console.log('color is not red or blue')
+        break;
+}
+*/
+
+/*
+// functions // arrow functions
+const addNums = (num1 = 1, num2 = 1) => num1 + num2
+console.log(addNums(5, 5))
+*/
+
+/*
+// OOP ES5
+// constructor function
+function Person(firstName, lastName, dob){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.dob = new Date(dob)
+
+    // this.getBirthYear = function(){
+    //     return this.dob.getFullYear()
+    // }
+
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+Person.prototype.getBirthYear = function(){
+    return this.dob.getFullYear()
+}
+
+// instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1980')
+const person2 = new Person('Mary', 'Anne', '4-3-2000')
+
+console.log(person2.getBirthYear())
+console.log(person1)
+*/
+
+
+/*
+// OOP ES6 / Syntactic Sugar
+// class
+class Person{
+    constructor(firstName, lastName, dob){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.dob = new Date(dob)
+    }
+
+    getBirthYear(){
+        return this.dob.getFullYear()
+    }
+
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+// instantiate object
+const person1 = new Person('John', 'Doe', '4-3-1980')
+const person2 = new Person('Mary', 'Anne', '4-3-2000')
+
+console.log(person2.getBirthYear())
+console.log(person1)
+*/
+
